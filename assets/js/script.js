d9bfbb031dcb7947a0a97bd0b39ee8fa25ec5917
rponce23 <rponcem75@mac.com>
2023-04-26 20:19:05 -0600

    added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,13 @@
 var today = dayjs();
 $('#currentDay').text(today.format('dddd, MMMM D YYYY, h:mm:ss a'));
-
 var nowHour = today.get('hour');
 console.log(nowHour); 
-var tableEl = $('#hourSch');
-var theChilds = tableEl.children();
-console.log("Length = " + theChilds.length);
-console.log(theChilds[2]);
-
 
 function colorTime(){
   $(".time-block").each(function () {
       var theHour = parseInt($(this).attr("id").split("hour")[1]);
       theHour = -theHour;
-      console.log(theHour, nowHour)
+      // console.log(theHour, nowHour)
 
       //check if we've moved past this time
       if (theHour < nowHour) {
@@ -34,6 +28,41 @@ function colorTime(){
   })
 };
 colorTime();
+
+$(".saveBtn").on("click", function () {
+  console.log(this);
+  var text = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+  localStorage.setItem(time, text);
+})
+
+function storageText(){
+  $("#hour-9 .description").val;
+  localStorage.getItem("hour-9");
+  $("#hour-10 .description").val;
+  localStorage.getItem("hour-10");
+  $("#hour-11 .description").val;
+  localStorage.getItem("hour-11");
+  $("#hour-12 .description").val;
+  localStorage.getItem("hour-12");
+  $("#hour-13 .description").val;
+  localStorage.getItem("hour-13");
+  $("#hour-14 .description").val;
+  localStorage.getItem("hour-14");
+  $("#hour-15 .description").val;
+  localStorage.getItem("hour-15");
+  $("#hour-16 .description").val;
+  localStorage.getItem("hour-16");
+  $("#hour-17 .description").val;
+  localStorage.getItem("hour-17");
+  $("#hour-18 .description").val;
+  localStorage.getItem("hour-18");
+  $("#hour-19 .description").val;
+  localStorage.getItem("hour-19");
+};
+storageText();
+
+
 
 
 
